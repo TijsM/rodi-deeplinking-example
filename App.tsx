c9098ea9a28571391a87329b1a80875efbd74b63
Tijs Martens <tijs@bothrs.com>
2022-02-20 20:44:58 +0100

    feat: add examples

---
+++ App.tsx
@@ -0,0 +1,57 @@
+import { Button, StyleSheet, View, Text } from "react-native";
+import * as Linking from "expo-linking";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <SubTitle text="direct linking" />
+      <StyledButton
+        text="open location (Ghent) in Rodi"
+        url="rodi://openCoordinate/?lat=51.0543422&lon=3.7174243"
+      />
+      <StyledButton
+        text="open location (Cupertino) in Rodi"
+        url="rodi://openCoordinate/?lat=51.0543422&lon=3.7174243"
+      />
+      <SubTitle text="linking with website fallback" />
+      <StyledButton
+        text="open location (Ghent) in Rodi"
+        url="https://rodi.app/open?lat=51.0543422&lon=3.7174243"
+      />
+      <StyledButton
+        text="open location (Cupertino) in Rodi"
+        url="https://rodi.app/open?lat=51.0543422&lon=3.7174243"
+      />
+    </View>
+  );
+}
+
+const StyledButton = ({ text, url }: { text: string; url: string }) => {
+  return (
+    <View
+      style={{
+        marginVertical: 12,
+      }}
+    >
+      <Button
+        title={text}
+        onPress={() => {
+          Linking.openURL(url);
+        }}
+      />
+    </View>
+  );
+};
+
+const SubTitle = ({ text }: { text: string }) => {
+  return <Text style={{ fontSize: 24, marginTop: 48 }}>{text}</Text>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
